8ec79d86b9845c4cb24fffa17fc6328e3f63b94f
Carter <cartergregg@icloud.com>
2025-06-23 20:56:05 -0700

    Initial Commit

---
+++ ClosetApp/ClosetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosetAppApp.swift
+//  ClosetApp
+//
+//  Created by Carter Gregg on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ClosetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClosetAppTests/ClosetAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClosetAppTests.swift
+//  ClosetAppTests
+//
+//  Created by Carter Gregg on 6/23/25.
+//
+
+import Testing
+@testable import ClosetApp
+
+struct ClosetAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
